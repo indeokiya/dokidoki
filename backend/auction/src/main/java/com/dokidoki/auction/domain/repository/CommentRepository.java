01b09de3f540f37294e28290108016a9ccfff6fe
윤재휘 <yjh200523@naver.com>
2023-03-13 14:40:30 +0900

    [be/refactor] delete 쿼리 최적화

--- backend/auction/src/main/java/com/dokidoki/auction/domain/repository/CommentRepository.java
+++ backend/auction/src/main/java/com/dokidoki/auction/domain/repository/CommentRepository.java
@@ -2,6 +2,9 @@ package com.dokidoki.auction.domain.repository;
 
 import com.dokidoki.auction.domain.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +12,8 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findCommentsByAuctionIdOrderByWrittenTime(Long auction_id);
-    int deleteCommentsByParentId(Long parent_id);
+
+    @Modifying
+    @Query("delete from Comment c where c.parentId=:parent_id")
+    int deleteCommentsByParentId(@Param("parent_id") Long parent_id);
 }
