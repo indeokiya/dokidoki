503c7320db7bffec59e9980bfa047cf8f510008e
윤재휘 <yjh200523@naver.com>
2023-03-14 17:03:38 +0900

    [be/feat] 사용자 제품 사진 업로드 관련 컴포넌트

---
+++ backend/auction/src/main/java/com/dokidoki/auction/domain/repository/AuctionImageRepository.java
@@ -0,0 +1,10 @@
+package com.dokidoki.auction.domain.repository;
+
+import com.dokidoki.auction.domain.entity.AuctionImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuctionImageRepository extends JpaRepository<AuctionImage, Long> {
+
+}
