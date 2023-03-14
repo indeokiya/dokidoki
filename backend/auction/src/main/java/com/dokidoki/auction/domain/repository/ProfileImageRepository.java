13138b55e783c84a6e09c51afa4e8d3b48cc33ca
윤재휘 <yjh200523@naver.com>
2023-03-14 17:04:37 +0900

    [be/feat] 사용자 프로필 사진 업로드 관련 컴포넌트

---
+++ backend/auction/src/main/java/com/dokidoki/auction/domain/repository/ProfileImageRepository.java
@@ -0,0 +1,10 @@
+package com.dokidoki.auction.domain.repository;
+
+import com.dokidoki.auction.domain.entity.ProfileImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileImageRepository extends JpaRepository<ProfileImage, Long> {
+
+}
