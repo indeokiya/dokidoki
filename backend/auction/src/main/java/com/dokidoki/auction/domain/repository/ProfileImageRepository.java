7a4b80fb938c8a7d6b1f6dcc9ddd12113e87fb2d
윤재휘 <yjh200523@naver.com>
2023-03-15 17:29:24 +0900

    [be/feat] 사용자 ID로 프로필 사진 조회 기능 추가

--- backend/auction/src/main/java/com/dokidoki/auction/domain/repository/ProfileImageRepository.java
+++ backend/auction/src/main/java/com/dokidoki/auction/domain/repository/ProfileImageRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProfileImageRepository extends JpaRepository<ProfileImage, Long> {
-
+    ProfileImage findProfileImageByMemberId(Long member_id);
 }
