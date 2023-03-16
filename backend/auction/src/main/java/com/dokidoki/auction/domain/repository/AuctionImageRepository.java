62a828971a07d8d1a6caa89a4e1f6012cc10509c
윤재휘 <yjh200523@naver.com>
2023-03-16 10:44:48 +0900

    [be/feat] 경매번호로 제품 사진 삭제하는 쿼리 설정

--- backend/auction/src/main/java/com/dokidoki/auction/domain/repository/AuctionImageRepository.java
+++ backend/auction/src/main/java/com/dokidoki/auction/domain/repository/AuctionImageRepository.java
@@ -2,6 +2,9 @@ package com.dokidoki.auction.domain.repository;
 
 import com.dokidoki.auction.domain.entity.AuctionImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +12,8 @@ import java.util.List;
 @Repository
 public interface AuctionImageRepository extends JpaRepository<AuctionImage, Long> {
     List<AuctionImage> findAuctionImagesByAuctionId(Long auctionId);
+
+    @Modifying
+    @Query("delete from AuctionImage a where a.auctionId=:auction_id")
+    void deleteAuctionImagesByAuctionId(@Param("auction_id") Long auction_id);
 }
