6e7f725d8c69984ba176112c984816941b37932f
윤재휘 <yjh200523@naver.com>
2023-03-16 10:18:27 +0900

    [be/refactor] 반환형 Optional로 설정

--- backend/auction/src/main/java/com/dokidoki/auction/domain/repository/ProfileImageRepository.java
+++ backend/auction/src/main/java/com/dokidoki/auction/domain/repository/ProfileImageRepository.java
@@ -4,7 +4,9 @@ import com.dokidoki.auction.domain.entity.ProfileImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProfileImageRepository extends JpaRepository<ProfileImage, Long> {
-    ProfileImage findProfileImageByMemberId(Long member_id);
+    Optional<ProfileImage> findProfileImageByMemberId(Long member_id);
 }
