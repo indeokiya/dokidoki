42d82deb3f57ee39076f2fa60ec12e90fe5f4f51
hjlim7831 <hjlim7831@gmail.com>
2023-03-16 17:58:22 +0900

    [be/feat] 개인정보 마스킹 기능 수정

--- backend/bid/src/main/java/com/dokidoki/bid/api/response/LeaderBoardMemberResp.java
+++ backend/bid/src/main/java/com/dokidoki/bid/api/response/LeaderBoardMemberResp.java
@@ -44,7 +44,7 @@ public class LeaderBoardMemberResp {
         StringBuilder sb = new StringBuilder();
         int indexOfAt = email.indexOf('@');
         sb.append(email.substring(0, 2));
-        for (int i = 0; i < indexOfAt - 1; i++) {
+        for (int i = 0; i < indexOfAt - 2; i++) {
             sb.append("*");
         }
         sb.append(email.substring(indexOfAt));
