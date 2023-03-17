f7daeabbea064246135df4db3c5dc3c5346651f3
tlsalsgpp12 <tlsalsgpp12@gmail.com>
2023-03-17 15:46:20 +0900

    [be/feat] member 조회 메서드 생성

---
+++ backend/auction/src/main/java/com/dokidoki/auction/service/MemberService.java
@@ -0,0 +1,35 @@
+package com.dokidoki.auction.service;
+
+import com.dokidoki.auction.common.error.exception.InvalidValueException;
+import com.dokidoki.auction.domain.entity.Member;
+import com.dokidoki.auction.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 멤버id로 member 정보 조회
+     * @param memberId
+     * @return  멤버정보
+     */
+    public Member getMemberById(Long memberId) {
+
+        // 사용자 유무 체크 (추후 필요한지 확인)
+        Optional<Member> memberO = memberRepository.findById(memberId);
+        if (memberO.isEmpty()) {
+            throw new InvalidValueException("해당 회원이 존재하지 않습니다.");
+        }
+
+//        return memberRepository.findById(memberId).get();
+        return memberO.get();
+    }
+
+
+}
