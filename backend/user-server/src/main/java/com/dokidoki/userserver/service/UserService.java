6624596904f985382bb27ef863ae1c3de28ea309
jongseok-oh <hynb14@gmail.com>
2023-03-20 11:13:26 +0900

    [be/feat] transactional 추가

--- backend/user-server/src/main/java/com/dokidoki/userserver/service/UserService.java
+++ backend/user-server/src/main/java/com/dokidoki/userserver/service/UserService.java
@@ -5,6 +5,7 @@ import com.dokidoki.userserver.enumtype.ProviderType;
 import com.dokidoki.userserver.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -13,17 +14,21 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public Optional<UserEntity> getUserFromSubAndProvider(String sub, ProviderType type){
         return userRepository.findBySubAndProviderType(sub,type);
     }
+    @Transactional
     public UserEntity saveUser(UserEntity user){
         return userRepository.save(user);
     }
 
+    @Transactional(readOnly = true)
     public Optional<UserEntity> getUserById(Long id){
         return userRepository.findById(id);
     }
 
+    @Transactional
     public void deleteById(Long id){
         userRepository.deleteById(id);
     }
