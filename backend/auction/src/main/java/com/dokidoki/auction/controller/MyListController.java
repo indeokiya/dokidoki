fbb7408881b0a6f2155a1d35e32958d59c2d4594
jongseok-oh <hynb14@gmail.com>
2023-03-21 17:38:29 +0900

    [infra/feat] server dockerize

---
+++ backend/auction/src/main/java/com/dokidoki/auction/controller/MyListController.java
@@ -0,0 +1,31 @@
+package com.dokidoki.auction.controller;
+
+import com.dokidoki.auction.common.JWTutil;
+import com.dokidoki.auction.dto.response.AuctionImageResponse;
+import com.dokidoki.auction.dto.response.CommonResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/my-lists")
+public class MyListController {
+
+    private final JWTutil jwTutil;
+
+//    @GetMapping("/bid")
+//    public ResponseEntity<?> readAuctionImages(HttpServletRequest request) {
+//
+//        Long userId = jwTutil.getUserId(request);
+//
+//
+//    }
+}
