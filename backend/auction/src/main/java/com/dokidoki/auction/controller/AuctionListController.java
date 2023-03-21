3c4840212579577a98bf9866d0e34cc94ad0df4f
윤재휘 <yjh200523@naver.com>
2023-03-21 15:10:23 +0900

    [be/feat] 경매 목록 조회 Controller & Service (전체적으로 리팩토링 및 최적화 필수)

--- backend/auction/src/main/java/com/dokidoki/auction/controller/AuctionListController.java
+++ backend/auction/src/main/java/com/dokidoki/auction/controller/AuctionListController.java
@@ -1,17 +1,20 @@
 package com.dokidoki.auction.controller;
 
 import com.dokidoki.auction.dto.response.CommonResponse;
+import com.dokidoki.auction.dto.response.PaginationResponse;
 import com.dokidoki.auction.dto.response.SimpleAuctionEndInfo;
+import com.dokidoki.auction.dto.response.SimpleAuctionIngInfo;
 import com.dokidoki.auction.service.AuctionListService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @RestController
@@ -21,13 +24,66 @@ import java.util.List;
 public class AuctionListController {
     private final AuctionListService auctionListService;
 
+    /*
+    메인 페이지 : 종료된 경매 목록 가져오기
+     */
     @GetMapping("/end")
-    public ResponseEntity<CommonResponse<List<SimpleAuctionEndInfo>>> readSimpleAuctionEnds(Pageable pageable) {
+    public ResponseEntity<CommonResponse<PaginationResponse<List<SimpleAuctionEndInfo>>>> readSimpleAuctionEnds(
+            @RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "10") Integer size) {
         // 데이터 조회
-        List<SimpleAuctionEndInfo> simpleAuctionEndInfos = auctionListService.readSimpleAuctionEnds(pageable);
+        PaginationResponse<List<SimpleAuctionEndInfo>> paginationResponse = auctionListService
+                .readSimpleAuctionEnd(PageRequest.of(page, size));
 
         return new ResponseEntity<>(
-                CommonResponse.of(200, "성공", simpleAuctionEndInfos),
+                CommonResponse.of(200, "종료된 경매 목록 조회 성공", paginationResponse),
+                HttpStatus.OK
+        );
+    }
+
+    /*
+    메인 페이지 : 진행중인 경매 목록 가져오기
+     */
+    @GetMapping("/in-progress")
+    public ResponseEntity<CommonResponse<PaginationResponse<List<SimpleAuctionIngInfo>>>> readSimpleAuctionIng(
+            @RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "10") Integer size) {
+        // 데이터 조회
+        PaginationResponse<List<SimpleAuctionIngInfo>> paginationResponse = auctionListService
+                .readSimpleAuctionIng(PageRequest.of(page, size));
+
+        return new ResponseEntity<>(
+                CommonResponse.of(200, "진행중인 경매 목록 조회 성공", paginationResponse),
+                HttpStatus.OK
+        );
+    }
+
+    /*
+    메인 페이지 : 마감임박 경매 목록 가져오기
+     */
+    @GetMapping("/deadline")
+    public ResponseEntity<CommonResponse<PaginationResponse<List<SimpleAuctionIngInfo>>>> readSimpleAuctionDeadline(
+            @RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "10") Integer size) {
+            // 데이터 조회
+            PaginationResponse<List<SimpleAuctionIngInfo>> paginationResponse = auctionListService
+                    .readSimpleAuctionDeadline(PageRequest.of(page, size));
+
+            return new ResponseEntity<>(
+                    CommonResponse.of(200, "마감임박 경매 목록 조회 성공", paginationResponse),
+                    HttpStatus.OK
+            );
+    }
+
+    /*
+    메인 페이지 : 키워드 및 카테고리 검색
+     */
+    @GetMapping("/search")
+    public ResponseEntity<CommonResponse<PaginationResponse<List<SimpleAuctionIngInfo>>>> searchSimpleAuctionIng(
+            @RequestParam(defaultValue = "") String keyword, @RequestParam(defaultValue = "0") Long category_id,
+            @RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "10") Integer size) {
+        PaginationResponse<List<SimpleAuctionIngInfo>> paginationResponse = auctionListService
+                .searchSimpleAuctionIng(keyword, category_id, PageRequest.of(page, size));
+
+        return new ResponseEntity<>(
+                CommonResponse.of(200, "경매 목록 검색 성공", paginationResponse),
                 HttpStatus.OK
         );
     }
--- backend/auction/src/main/java/com/dokidoki/auction/service/AuctionListService.java
+++ backend/auction/src/main/java/com/dokidoki/auction/service/AuctionListService.java
@@ -1,17 +1,14 @@
 package com.dokidoki.auction.service;
 
-import com.dokidoki.auction.domain.entity.AuctionImageEntity;
 import com.dokidoki.auction.domain.repository.AuctionEndRepository;
-import com.dokidoki.auction.domain.repository.AuctionImageRepository;
-import com.dokidoki.auction.dto.response.AuctionImageResponse;
-import com.dokidoki.auction.dto.response.SimpleAuctionEndInfo;
-import com.dokidoki.auction.dto.response.SimpleAuctionEndInterface;
+import com.dokidoki.auction.domain.repository.AuctionIngRepository;
+import com.dokidoki.auction.dto.response.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,14 +17,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AuctionListService {
     private final AuctionEndRepository auctionEndRepository;
+    private final AuctionIngRepository auctionIngRepository;
     private final ImageService imageService;
 
-    public List<SimpleAuctionEndInfo> readSimpleAuctionEnds(Pageable pageable) {
+    /*
+    종료된 경매 목록 조회
+     */
+    public PaginationResponse<List<SimpleAuctionEndInfo>> readSimpleAuctionEnd(Pageable pageable) {
         // 데이터 조회
-        Page<List<SimpleAuctionEndInterface>> simpleAuctionEndInterfaces = auctionEndRepository
-                .findSimpleAuctionEndEntitiesById(pageable);
+        Page<SimpleAuctionEndInterface> simpleAuctionEndInterfaces = auctionEndRepository
+                .findAllSimpleEndList(pageable);
 
-        // DTO 생성
+        // 데이터 조합
         List<SimpleAuctionEndInfo> simpleAuctionEndInfos = new ArrayList<>();
         for (SimpleAuctionEndInterface simpleAuctionEndInterface : simpleAuctionEndInterfaces) {
             // 경매 제품 사진 검색
@@ -39,10 +40,82 @@ public class AuctionListService {
                     new SimpleAuctionEndInfo(
                             simpleAuctionEndInterface,
                             auctionImageResponse.getImage_urls()
-                    );
-            )
+                    )
+            );
         }
 
-        return simpleAuctionEndInfos;
+        // Response DTO 생성 및 반환
+        return new PaginationResponse<>(
+                simpleAuctionEndInfos,
+                simpleAuctionEndInterfaces.isLast()
+        );
+    }
+
+    /*
+    진행중인 전체 경매 목록 조회
+     */
+    public PaginationResponse<List<SimpleAuctionIngInfo>> readSimpleAuctionIng(Pageable pageable) {
+        // 데이터 조회
+        Page<SimpleAuctionIngInterface> simpleAuctionIngInterfaces = auctionIngRepository
+                .findAllSimpleIngList(pageable);
+        return convertToDTOWithImages(simpleAuctionIngInterfaces);
+    }
+
+    /*
+    진행중인 마감임박 경매 목록 조회
+     */
+    public PaginationResponse<List<SimpleAuctionIngInfo>> readSimpleAuctionDeadline(Pageable pageable) {
+        // 데이터 조회
+        Page<SimpleAuctionIngInterface> simpleAuctionIngInterfaces = auctionIngRepository
+                .findAllSimpleDeadlineList(pageable);
+        return convertToDTOWithImages(simpleAuctionIngInterfaces);
+    }
+
+    /*
+    진행중인 경매 목록 검색
+     */
+    public PaginationResponse<List<SimpleAuctionIngInfo>> searchSimpleAuctionIng(
+            String keyword, Long categoryId, Pageable pageable) {
+        // 불필요 문자 제거
+        keyword = keyword.strip();
+
+        // 데이터 조회, 카테고리 설정 여부에 따라 (키워드, 카테고리) 검색과 (키워드) 검색으로 분기
+        Page<SimpleAuctionIngInterface> simpleAuctionIngInterfaces = null;
+        if (categoryId == 0)
+            simpleAuctionIngInterfaces = auctionIngRepository
+                    .findAllSimpleIngListByKeyword(keyword, pageable);
+        else
+            simpleAuctionIngInterfaces = auctionIngRepository
+                    .findAllSimpleIngListByKeywordANDCategoryId(keyword, categoryId, pageable);
+
+        return convertToDTOWithImages(simpleAuctionIngInterfaces);
+    }
+
+    /*
+    DB에서 조회한 진행중인 경매 목록 데이터에 제품 이미지를 추가하며 DTO로 변환
+     */
+    public PaginationResponse<List<SimpleAuctionIngInfo>> convertToDTOWithImages(
+            Page<SimpleAuctionIngInterface> simpleAuctionIngInterfaces) {
+        // 데이터 조합
+        List<SimpleAuctionIngInfo> simpleAuctionIngInfos = new ArrayList<>();
+        for (SimpleAuctionIngInterface simpleAuctionIngInterface : simpleAuctionIngInterfaces) {
+            // 경매 제품 사진 검색
+            AuctionImageResponse auctionImageResponse = imageService
+                    .readAuctionImages(simpleAuctionIngInterface.getAuction_id());
+
+            // Response DTO 담기
+            simpleAuctionIngInfos.add(
+                    new SimpleAuctionIngInfo(
+                            simpleAuctionIngInterface,
+                            auctionImageResponse.getImage_urls()
+                    )
+            );
+        }
+
+        // Response DTO 생성 및 반환
+        return new PaginationResponse<>(
+                simpleAuctionIngInfos,
+                simpleAuctionIngInterfaces.isLast()
+        );
     }
 }
