e60f387f9ed27be75f0a18c8853c34fd6db440c1
윤재휘 <yjh200523@naver.com>
2023-03-21 15:08:25 +0900

    [be/feat] 종료된 경매 목록 조회

--- backend/auction/src/main/java/com/dokidoki/auction/domain/repository/AuctionEndRepository.java
+++ backend/auction/src/main/java/com/dokidoki/auction/domain/repository/AuctionEndRepository.java
@@ -4,12 +4,12 @@ import com.dokidoki.auction.domain.entity.AuctionEndEntity;
 import com.dokidoki.auction.dto.response.DetailAuctionEndInterface;
 import com.dokidoki.auction.dto.response.SimpleAuctionEndInterface;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
@@ -31,17 +31,14 @@ public interface AuctionEndRepository extends JpaRepository<AuctionEndEntity, Lo
             " AND a.buyer_id = b.id", nativeQuery = true)
     List<DetailAuctionEndInterface> findDetailAuctionEndEntitiesById(@Param("auction_id") Long auctionId);
 
-    @Query(value = "SELECT a.id as auction_id, a.title as auction_title, a.start_time as start_time" +
-            " , a.end_time as end_time, p.name as product_name, c.category_name as category_name" +
-            " , a.offer_price as offer_price, a.final_price as final_price" +
-            " , b.name as buyer_name " +
-            "FROM auction_end a" +
-            " JOIN product p" +
-            " JOIN category c" +
-            " JOIN member b " +
-            "WHERE a.id = :auction_id" +
-            " AND a.product_id = p.id" +
-            " AND p.category_id = c.id" +
-            " AND a.buyer_id = b.id", nativeQuery = true)
-    Page<List<SimpleAuctionEndInterface>> findSimpleAuctionEndEntitiesById(Pageable pageable);
+    @Query(value = "SELECT a.id as auction_id, a.title as auction_title, a.start_time as start_time " +
+            " , a.end_time as end_time, p.name as product_name, c.category_name as category_name " +
+            " , a.offer_price as offer_price, a.final_price as final_price, a.buyer_id as buyer_id " +
+            "FROM auction_end a " +
+            " JOIN product p " +
+            " JOIN category c " +
+            "WHERE a.product_id = p.id" +
+            " AND p.category_id = c.id " +
+            "ORDER BY a.end_time DESC ", nativeQuery = true)
+    Page<SimpleAuctionEndInterface> findAllSimpleEndList(Pageable pageable);
 }
