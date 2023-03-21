bb2a3272feb564fd158e303f95c9b23bad8c083b
윤재휘 <yjh200523@naver.com>
2023-03-21 15:09:15 +0900

    [be/feat] 전체, 마감임박, 키워드 검색 등 진행중인 경매 목록 조회

---
+++ backend/auction/src/main/java/com/dokidoki/auction/dto/response/SimpleAuctionIngInterface.java
@@ -0,0 +1,13 @@
+package com.dokidoki.auction.dto.response;
+
+import java.time.LocalDateTime;
+
+public interface SimpleAuctionIngInterface {
+    Long getAuction_id();
+    String getAuction_title();
+    LocalDateTime  getEnd_time();
+    String getProduct_name();
+    String getCategory_name();
+    Integer getOffer_price();
+    Integer getCur_price();
+}
