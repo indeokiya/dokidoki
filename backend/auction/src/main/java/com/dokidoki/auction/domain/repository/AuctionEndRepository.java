0a27a62307b344f13edd610dfd533aa0c7ada431
윤재휘 <yjh200523@naver.com>
2023-03-22 13:12:03 +0900

    [be/refactor] SQL -> JPQL 변환

--- backend/auction/src/main/java/com/dokidoki/auction/domain/repository/AuctionEndRepository.java
+++ backend/auction/src/main/java/com/dokidoki/auction/domain/repository/AuctionEndRepository.java
@@ -14,31 +14,24 @@ import java.util.List;
 
 @Repository
 public interface AuctionEndRepository extends JpaRepository<AuctionEndEntity, Long> {
-    // 경매 End, 제품, 카테고리, 사용자 테이블 조인해서 데이터 검색하기 (최적화 필요할 것 같긴 한데 일단은..)
-    @Query(value = "SELECT a.title as auction_title, a.start_time as start_time, a.end_time as end_time" +
-            " , p.name as product_name, c.category_name as category_name, a.description as description" +
-            " , a.offer_price as offer_price, a.final_price as final_price" +
-            " , s.name as seller_name, b.name as buyer_name " +
-            "FROM auction_end a" +
-            " JOIN product p" +
-            " JOIN category c" +
-            " JOIN member s" +
-            " JOIN member b " +
-            "WHERE a.id = :auction_id" +
-            " AND a.product_id = p.id" +
-            " AND p.category_id = c.id" +
-            " AND a.seller_id = s.id" +
-            " AND a.buyer_id = b.id", nativeQuery = true)
+    // 경매 End, 제품, 카테고리, 사용자 테이블 조인해서 데이터 검색하기
+    @Query("SELECT a.title as auction_title, a.startTime as start_time, a.endTime as end_time, a.description as description " +
+            ", a.offerPrice as offer_price, a.finalPrice as final_price, s.name as seller_name, b.name as buyer_name " +
+            ", p.name as product_name, c.categoryName as category_name " +
+            "FROM AuctionEndEntity a " +
+            " JOIN a.product p " +
+            " JOIN p.categoryEntity c " +
+            " JOIN a.buyer b " +
+            " JOIN a.seller s " +
+            "WHERE a.id = :auction_id")
     List<DetailAuctionEndInterface> findDetailAuctionEndEntitiesById(@Param("auction_id") Long auctionId);
-
-    @Query(value = "SELECT a.id as auction_id, a.title as auction_title, a.start_time as start_time " +
-            " , a.end_time as end_time, p.name as product_name, c.category_name as category_name " +
-            " , a.offer_price as offer_price, a.final_price as final_price, a.buyer_id as buyer_id " +
-            "FROM auction_end a " +
-            " JOIN product p " +
-            " JOIN category c " +
-            "WHERE a.product_id = p.id" +
-            " AND p.category_id = c.id " +
-            "ORDER BY a.end_time DESC ", nativeQuery = true)
+    
+    @Query("SELECT a.id as auction_id, a.title as auction_title, a.startTime as start_time, a.endTime as end_time" +
+            ", a.offerPrice as offer_price, a.finalPrice as final_price, a.buyer.id as buyer_id" +
+            ", p.name as product_name, c.categoryName as category_name " +
+            "FROM AuctionEndEntity a " +
+            " JOIN a.product p " +
+            " JOIN p.categoryEntity c " +
+            "ORDER BY a.endTime DESC ")
     Page<SimpleAuctionEndInterface> findAllSimpleEndList(Pageable pageable);
 }
