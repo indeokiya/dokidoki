69ff4b16a6bb24e80253948df0fc82590f224796
hjlim7831 <hjlim7831@gmail.com>
2023-03-27 21:48:49 +0900

    [be/feat] 종료된 경매 Exception 추가, 이전 최고 입찰자 정보 전달 추가

--- backend/bid/src/main/java/com/dokidoki/bid/kafka/dto/KafkaBidDTO.java
+++ backend/bid/src/main/java/com/dokidoki/bid/kafka/dto/KafkaBidDTO.java
@@ -1,5 +1,7 @@
 package com.dokidoki.bid.kafka.dto;
 
+import com.dokidoki.bid.api.request.AuctionBidReq;
+import com.dokidoki.bid.api.response.LeaderBoardMemberResp;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +14,7 @@ import java.time.LocalDateTime;
 @ToString
 public class KafkaBidDTO {
 
+    private long beforeWinnerId;
     private long memberId;
     private long auctionId;
     private String name;
@@ -19,12 +22,25 @@ public class KafkaBidDTO {
     private LocalDateTime bidTime;
     public KafkaBidDTO() {}
 
+    public static KafkaBidDTO of(long auctionId, LeaderBoardMemberResp resp, AuctionBidReq req, long memberId, long beforeWinnerId) {
+        KafkaBidDTO dto = KafkaBidDTO.builder()
+                .beforeWinnerId(beforeWinnerId)
+                .memberId(memberId)
+                .auctionId(auctionId)
+                .name(req.getName())
+                .highestPrice(resp.getBidPrice())
+                .bidTime(resp.getBidTime())
+                .build();
+        return dto;
+    }
+
     @Builder
-    public KafkaBidDTO(long memberId, long auctionId, String name, int highestPrice, LocalDateTime bidTime) {
+    public KafkaBidDTO(long memberId, long auctionId, String name, int highestPrice, LocalDateTime bidTime, long beforeWinnerId) {
         this.memberId = memberId;
         this.auctionId = auctionId;
         this.name = name;
         this.highestPrice = highestPrice;
         this.bidTime = bidTime;
+        this.beforeWinnerId = beforeWinnerId;
     }
 }
