9b405a3aeb9c1343965aaf4a99560fdeaf350515
hjlim7831 <hjlim7831@gmail.com>
2023-03-27 17:59:29 +0900

    [be/fix] null로 넘어오는 경우 해결 (진행중)

--- backend/bid/src/main/java/com/dokidoki/bid/api/response/LeaderBoardMemberResp.java
+++ backend/bid/src/main/java/com/dokidoki/bid/api/response/LeaderBoardMemberResp.java
@@ -28,7 +28,13 @@ public class LeaderBoardMemberResp {
     public static LeaderBoardMemberResp of(LeaderBoardMemberInfo info, int bidPrice) {
         // 개인정보 가리기
         String name = info.getName();
-        String modifiedName = name.substring(0, 1) + "*" + name.substring(2);
+        String modifiedName;
+        if (name.length() >= 2) {
+            modifiedName = name.substring(0, 1) + "*" + name.substring(2);
+        } else {
+            modifiedName = name;
+        }
+
         String modifiedEmail = getModifiedEmail(info.getEmail());
 
         return LeaderBoardMemberResp.builder()
