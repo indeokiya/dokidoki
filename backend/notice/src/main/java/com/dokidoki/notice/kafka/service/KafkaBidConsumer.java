9936f37ad9ccb93bebedbbcd59fa109118e8587a
인덕 <indeokiya@snu.ac.kr>
2023-03-28 09:58:02 +0900

    [be/fix] notice 서버 카프카 컨슈머 수정

--- backend/notice/src/main/java/com/dokidoki/notice/kafka/service/KafkaBidConsumer.java
+++ backend/notice/src/main/java/com/dokidoki/notice/kafka/service/KafkaBidConsumer.java
@@ -1,8 +1,13 @@
 package com.dokidoki.notice.kafka.service;
 
+import com.dokidoki.notice.api.service.NoticeService;
+import com.dokidoki.notice.kafka.dto.KafkaAuctionEndDTO;
 import com.dokidoki.notice.kafka.dto.KafkaAuctionRegisterDTO;
+import com.dokidoki.notice.kafka.dto.KafkaAuctionUpdateDTO;
+import com.dokidoki.notice.kafka.dto.KafkaBidDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
@@ -14,35 +19,48 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class KafkaBidConsumer {
 
-    @KafkaListener(topics = "${spring.kafka.auctionRegisterConfig.topic}", containerFactory = "auctionRegisterKafkaListenerContainerFactory")
-    public void auctionRegisterListener(
-            @Payload KafkaAuctionRegisterDTO dto,
-            @Headers MessageHeaders headers) {
-        System.out.println("Received auction register message : " + dto);
-        System.out.print("header | ");
-        headers.keySet().forEach(key -> {
-//            log.debug("header | key: [{}] value: [{}]", key, headers.get(key));
-            System.out.print("{" + key + ", " + headers.get(key) + "} ");
-        });
-        System.out.println("");
+    @Autowired
+    NoticeService noticeService;
 
-        System.out.println();
+    @KafkaListener(topics = "${spring.kafka.auctionUpdateConfig.topic}", containerFactory = "auctionUpdateKafkaListenerContainerFactory")
+    public void auctionUpdateListener(
+            @Payload KafkaAuctionUpdateDTO dto,
+            @Headers MessageHeaders headers) {
+        System.out.println("Received auction update message : " + dto);
+        log.info("Received auction update message: [{}]", dto);
+        headers.keySet().forEach(key -> {
+            log.info("header | key: [{}] value: [{}]", key, headers.get(key));
+        });
 
         // auction : memberid, name, email .....
         // websocket method_bid_success(memberid, name, email)
-
     }
-//
-//    @KafkaListener(topics = "${spring.kafka.auctionUpdateConfig.topic}", containerFactory = "auctionUpdateKafkaListenerContainerFactory")
-//    public void auctionUpdateListener(
-//            @Payload KafkaAuctionUpdateDTO update,
-//            @Headers MessageHeaders headers) {
-//        System.out.println("Received auction update message : " + update);
-//        System.out.print("header | ");
-//        headers.keySet().forEach(key -> {
-////            log.debug("header | key: [{}] value: [{}]", key, headers.get(key));
-//            System.out.print("{" + key + ", " + headers.get(key) + "} ");
-//        });
-//        System.out.println("");
-//    }
+
+    @KafkaListener(topics = "${spring.kafka.auctionEndConfig.topic}", containerFactory = "auctionEndKafkaListenerContainerFactory")
+    public void auctionEndListener(
+            @Payload KafkaAuctionEndDTO dto,
+            @Headers MessageHeaders headers) {
+        System.out.println("Received auction end message : " + dto);
+        log.info("Received auction end message: [{}]", dto);
+        headers.keySet().forEach(key -> {
+            log.info("header | key: [{}] value: [{}]", key, headers.get(key));
+        });
+
+        noticeService.auctionComplete(dto);
+        noticeService.auctionSuccess(dto);
+        noticeService.auctionFail(dto);
+    }
+
+    @KafkaListener(topics = "${spring.kafka.bidConfig.topic}", containerFactory = "bidKafkaListenerContainerFactory")
+    public void bidListener(
+            @Payload KafkaBidDTO dto,
+            @Headers MessageHeaders headers) {
+        System.out.println("Received auction end message : " + dto);
+        log.info("Received auction end message: [{}]", dto);
+        headers.keySet().forEach(key -> {
+            log.info("header | key: [{}] value: [{}]", key, headers.get(key));
+        });
+
+        noticeService.auctionOutBid(dto);
+    }
 }
