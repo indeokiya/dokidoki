09cd4f9c92712758832affd75e2fbc276d3155a3
jongseok-oh <hynb14@gmail.com>
2023-03-28 09:31:40 +0900

    [fe/fix] 회원 가입 버튼 삭제, 주석 변경

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -31,7 +31,7 @@ bidAPI.defaults.timeout = 3000;
 function addRequestIntercepter(axiosApi : any){
   axiosApi.interceptors.request.use(
     (config:any) => {
-        // 세션 스토리지에서 Access Token 가져오기, 없다면 Undefined
+        // 로컬 스토리지에서 Access Token 가져오기, 없다면 Undefined
         let accessToken = localStorage.getItem("access_token");
 
         // Authorization 헤더에 토큰 추가 및 credential 설정
