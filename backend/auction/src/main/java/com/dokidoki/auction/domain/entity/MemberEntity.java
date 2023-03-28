1a3e2f0913d507a7b70ea8097996a51faaa3e580
윤재휘 <yjh200523@naver.com>
2023-03-28 13:30:50 +0900

    [be/feat] Response DTO 수정

--- backend/auction/src/main/java/com/dokidoki/auction/domain/entity/MemberEntity.java
+++ backend/auction/src/main/java/com/dokidoki/auction/domain/entity/MemberEntity.java
@@ -15,4 +15,5 @@ public class MemberEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String picture;
 }
--- backend/auction/src/main/java/com/dokidoki/auction/dto/response/CommentResponse.java
+++ backend/auction/src/main/java/com/dokidoki/auction/dto/response/CommentResponse.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +15,7 @@ import java.util.List;
 public class CommentResponse {
     private final Long id;
     private final Long member_id;
+    private final String member_profile;
     private final String member_name;
     private final String content;
     private final LocalDateTime written_time;
@@ -25,6 +25,7 @@ public class CommentResponse {
     public CommentResponse(CommentEntity commentEntity) {
         this.id = commentEntity.getId();
         this.member_id = commentEntity.getMemberEntity().getId();
+        this.member_profile = commentEntity.getMemberEntity().getPicture();
         this.member_name = commentEntity.getMemberEntity().getName();
         this.content = commentEntity.getContent();
         this.written_time = commentEntity.getWrittenTime();
