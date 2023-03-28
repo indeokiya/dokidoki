bdaa342e74cc88830704e99f80a75609e1ebc8a7
hjlim7831 <hjlim7831@gmail.com>
2023-03-28 17:46:50 +0900

    [be/feat] AuctionSocketService 구현, Controller와 연결

--- backend/notice/src/main/java/com/dokidoki/notice/api/response/SocketPriceSizeResp.java
+++ backend/notice/src/main/java/com/dokidoki/notice/api/response/SocketPriceSizeResp.java
@@ -1,4 +1,4 @@
-package com.dokidoki.notice.api.dto;
+package com.dokidoki.notice.api.response;
 
 import com.dokidoki.notice.kafka.dto.KafkaAuctionUpdateDTO;
 import lombok.AllArgsConstructor;
@@ -8,15 +8,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class SocketPriceSizeDTO {
+public class SocketPriceSizeResp {
     private String type;
     private int priceSize;
-    public static SocketPriceSizeDTO from(KafkaAuctionUpdateDTO dto) {
-        SocketPriceSizeDTO resDTO = SocketPriceSizeDTO.builder()
+    public static SocketPriceSizeResp from(KafkaAuctionUpdateDTO dto) {
+        SocketPriceSizeResp resp = SocketPriceSizeResp.builder()
                 .type("price_size")
                 .priceSize(dto.getPriceSize()).build();
 
-        return resDTO;
+        return resp;
 
     }
 
