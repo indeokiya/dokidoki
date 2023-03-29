24b9b696077403ef283bc6e94f29c7d6507b74b1
jongseok-oh <hynb14@gmail.com>
2023-03-29 10:05:58 +0900

    [fe/feat] main total price

--- frontend/src/components/main/TotalTransaction/TotalTransaction.tsx
+++ frontend/src/components/main/TotalTransaction/TotalTransaction.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import {auctionAPI} from "../../../api/axios";
+import { useState , useEffect} from 'react';
 
 const TotalTransaction = () => {
   const StyledDiv = styled.div`
@@ -25,12 +27,26 @@ const TotalTransaction = () => {
     padding-bottom: 5px;
   `;
 
+  function numberFormat(price: number | null) {
+    return price?.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',') + ' 원';
+  }
+
+  const [totalPrice, setTotalPrice] = useState(0)
+  
+  setTimeout(()=>{
+    auctionAPI.get(
+      "/total-prices"
+    ).then(({data})=>{
+      setTotalPrice(pre => data)
+    })
+  },1000)
+
   return (
     <>
       <StyledDiv>
         <AttachMoneyIcon fontSize="large" sx={{ marginBottom: '15px' }} />
         <StyledSmallP>총 거래된 금액</StyledSmallP>
-        <StyledLargeP>123,1241,212,315 원</StyledLargeP>
+        <StyledLargeP>{numberFormat(totalPrice)}</StyledLargeP>
       </StyledDiv>
     </>
   );
