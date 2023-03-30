2b9d4c45451aeccdeefb68ee1454a1acf52436af
HwiHwi <yjh200523@naver.com>
2023-03-30 22:19:48 +0900

    [be/refactor] History DTO, 경매 제목 및 이미지 URL 추가

--- backend/auction/src/main/java/com/dokidoki/auction/dto/response/MyHistoryInfo.java
+++ backend/auction/src/main/java/com/dokidoki/auction/dto/response/MyHistoryInfo.java
@@ -9,6 +9,8 @@ import java.time.LocalDateTime;
 public class MyHistoryInfo {
     private final Long auction_id;
 
+    private final String auction_title;
+
     private final String buyer_name;
     private final String seller_name;
 
@@ -18,17 +20,20 @@ public class MyHistoryInfo {
     private final String category_name;
     private final String product_name;
 
+    private final String auction_image_url;
+
     private final Integer year, month, day;
 
-    public MyHistoryInfo(AuctionEndEntity auctionEndEntity) {
+    public MyHistoryInfo(AuctionEndEntity auctionEndEntity, String auctionImageUrl) {
         this.auction_id = auctionEndEntity.getId();
+        this.auction_title = auctionEndEntity.getTitle();
         this.buyer_name = auctionEndEntity.getBuyer().getName();
         this.seller_name = auctionEndEntity.getSeller().getName();
         this.offer_price = auctionEndEntity.getOfferPrice();
         this.final_price = auctionEndEntity.getFinalPrice();
         this.category_name = auctionEndEntity.getProduct().getCategoryEntity().getCategoryName();
         this.product_name = auctionEndEntity.getProduct().getName();
-
+        this.auction_image_url = auctionImageUrl;
         LocalDateTime endDate = auctionEndEntity.getEndTime();
         this.year = endDate.getYear();
         this.month = endDate.getMonthValue();
