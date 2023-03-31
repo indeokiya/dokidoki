68cf03dca3a7f860b6b87eaefbc6215d1cdaae41
hjlim7831 <hjlim7831@gmail.com>
2023-03-31 12:13:10 +0900

    [be/feat] bid 서버 mysql 연결 삭제

--- backend/bid/src/main/java/com/dokidoki/bid/BidApplication.java
+++ backend/bid/src/main/java/com/dokidoki/bid/BidApplication.java
@@ -3,10 +3,8 @@ package com.dokidoki.bid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableAspectJAutoProxy
 public class BidApplication {
 	public static void main(String[] args) {
