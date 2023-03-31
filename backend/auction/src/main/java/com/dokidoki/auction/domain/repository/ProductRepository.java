70b97a074fdb0f622c8641bd93add3227be2e624
윤재휘 <yjh200523@naver.com>
2023-03-31 10:52:19 +0900

    [be/refactor] 제품 조회 시 검색결과 10개 제한

--- backend/auction/src/main/java/com/dokidoki/auction/domain/repository/ProductRepository.java
+++ backend/auction/src/main/java/com/dokidoki/auction/domain/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.dokidoki.auction.domain.repository;
 
 import com.dokidoki.auction.domain.entity.ProductEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,5 +13,5 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     @Query("SELECT p " +
             "FROM ProductEntity p " +
             "WHERE p.name LIKE %:keyword% OR p.categoryEntity.categoryName LIKE %:keyword% ")
-    List<ProductEntity> findByKeyword(@Param("keyword") String keyword);
+    List<ProductEntity> findByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
