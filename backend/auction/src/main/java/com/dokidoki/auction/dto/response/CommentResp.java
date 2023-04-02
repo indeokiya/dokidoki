0f39bbf73999b5c9e198bf6944dcd4fea4912596
HwiHwi <yjh200523@naver.com>
2023-04-02 18:33:03 +0900

    [be/feat] 댓글 삭제 시 하위 댓글은 사라지지 않도록 설정

--- backend/auction/src/main/java/com/dokidoki/auction/dto/response/CommentResp.java
+++ backend/auction/src/main/java/com/dokidoki/auction/dto/response/CommentResp.java
@@ -1,6 +1,7 @@
 package com.dokidoki.auction.dto.response;
 
 import com.dokidoki.auction.domain.entity.CommentEntity;
+import com.dokidoki.auction.domain.entity.MemberEntity;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -23,11 +24,23 @@ public class CommentResp {
     private final List<CommentResp> sub_comments;
 
     public CommentResp(CommentEntity commentEntity) {
+        Long memberId = -1L;
+        String memberProfile = null;
+        String memberName = "";
+        String content = "삭제된 댓글입니다.";
+        MemberEntity memberEntity = commentEntity.getMemberEntity();;
+        if (memberEntity != null) {
+            memberId = memberEntity.getId();
+            memberName = memberEntity.getName();
+            memberProfile = memberEntity.getPicture();
+            content = commentEntity.getContent();
+        }
+
         this.id = commentEntity.getId();
-        this.member_id = commentEntity.getMemberEntity().getId();
-        this.member_profile = commentEntity.getMemberEntity().getPicture();
-        this.member_name = commentEntity.getMemberEntity().getName();
-        this.content = commentEntity.getContent();
+        this.member_id = memberId;
+        this.member_profile = memberProfile;
+        this.member_name = memberName;
+        this.content = content;
         this.written_time = commentEntity.getWrittenTime();
         this.modified_time = commentEntity.getModifiedTime();
         this.sub_comments = new ArrayList<>();
