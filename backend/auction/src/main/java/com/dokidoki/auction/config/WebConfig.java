5f03d0a226fcf0dee17759d790c5a8f343679aa7
jongseok-oh <hynb14@gmail.com>
2023-04-03 11:45:32 +0900

    [be/fix] allowed cors localhost:3000

--- backend/auction/src/main/java/com/dokidoki/auction/config/WebConfig.java
+++ backend/auction/src/main/java/com/dokidoki/auction/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.dokidoki.auction.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,9 +11,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://j8a202.p.ssafy.io")
-                .allowedHeaders("*")
-                .allowedMethods("*")
+                .allowedOriginPatterns("http://localhost:3000", "https://j8a202.p.ssafy.io")
+                .allowedHeaders(HttpHeaders.AUTHORIZATION)
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                )
                 .allowCredentials(true);
     }
 }
