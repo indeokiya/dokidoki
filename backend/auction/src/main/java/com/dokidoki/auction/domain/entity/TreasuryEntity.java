a89d760b8f1074e7041b23502190cd73ab2e200a
윤재휘 <yjh200523@naver.com>
2023-04-04 09:35:17 +0900

    [be/fix] 경매 종료 이벤트에서 TreasuryEntity 생성 시 발생하는 'No default constructor...' 오류 수정

--- backend/auction/src/main/java/com/dokidoki/auction/domain/entity/TreasuryEntity.java
+++ backend/auction/src/main/java/com/dokidoki/auction/domain/entity/TreasuryEntity.java
@@ -1,20 +1,22 @@
 package com.dokidoki.auction.domain.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Table(name = "treasury")
 @Getter
-@Builder
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class TreasuryEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long money;
+
+    @Builder
+    public TreasuryEntity(Long id, Long money) {
+        this.id = id;
+        this.money = money;
+    }
 }
