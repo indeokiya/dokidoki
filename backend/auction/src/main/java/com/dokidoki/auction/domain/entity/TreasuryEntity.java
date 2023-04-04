07ae70275ce01e645c543fb8ca198a7bef57ec2e
윤재휘 <yjh200523@naver.com>
2023-04-04 10:07:53 +0900

    [be/feat] Treasury Table에 사용자별 수수료 데이터를 저장하도록 변경

--- backend/auction/src/main/java/com/dokidoki/auction/domain/entity/TreasuryEntity.java
+++ backend/auction/src/main/java/com/dokidoki/auction/domain/entity/TreasuryEntity.java
@@ -1,6 +1,8 @@
 package com.dokidoki.auction.domain.entity;
 
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
@@ -12,11 +14,18 @@ public class TreasuryEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private MemberEntity member;
+
     private Long money;
 
     @Builder
-    public TreasuryEntity(Long id, Long money) {
+    public TreasuryEntity(Long id, MemberEntity member, Long money) {
         this.id = id;
+        this.member = member;
         this.money = money;
     }
 }
