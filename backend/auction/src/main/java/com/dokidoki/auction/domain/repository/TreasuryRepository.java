46d2520ce72717cfdd1c7ac8655c988e285922f4
윤재휘 <yjh200523@naver.com>
2023-04-04 11:12:08 +0900

    [be/feat] 국고 조회 API

--- backend/auction/src/main/java/com/dokidoki/auction/domain/repository/TreasuryRepository.java
+++ backend/auction/src/main/java/com/dokidoki/auction/domain/repository/TreasuryRepository.java
@@ -3,6 +3,9 @@ package com.dokidoki.auction.domain.repository;
 import com.dokidoki.auction.domain.entity.ProductEntity;
 import com.dokidoki.auction.domain.entity.TreasuryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TreasuryRepository extends JpaRepository<TreasuryEntity, Long> {
+    @Query("SELECT SUM(t.money) FROM TreasuryEntity t")
+    Long getTotalTreasury();
 }
