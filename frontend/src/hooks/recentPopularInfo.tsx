97a8d368f4db3729ebfdafaad798a1ae65e84755
인덕 <indeokiya@snu.ac.kr>
2023-04-05 23:38:57 +0900

    [fe/fix]: fetch 결과가 없을 시 더미데이터 추가

--- frontend/src/hooks/recentPopularInfo.tsx
+++ frontend/src/hooks/recentPopularInfo.tsx
@@ -18,6 +18,7 @@ export const useRecentPopularInfo = () => {
     staleTime: 30*1000,
     cacheTime: 3*30*1000,
     retry: 0,
+    select,
     refetchOnMount:true,
     refetchOnWindowFocus:false,
     refetchInterval: 30*1000,  
@@ -25,13 +26,16 @@ export const useRecentPopularInfo = () => {
 
 }
 
-const onSuccess = () => {
-  console.log("success")
-}
-
 // select로 fetch 해온 데이터 가공
 const select = (data: any) => {
-  console.log("select >> ", data)
+  if (data.bid.length === 0) {
+    data.bid = dummyData;
+    console.log("dummyData added to bid!")
+  }
+  if (data.click.length === 0) {
+    data.click = dummyData;
+    console.log("dummyData added to click!")
+  }
   return data;
 }
 
@@ -40,3 +44,11 @@ const onError = (err: any) => {
   // err.alterData = alterData;
   return err;
 }
+
+const dummyData: any[] = [
+  {auction_id: 70, auction_title: "삼성전자 ❄무풍 에어컨❄", category_name: "BESPOKE", cnt: 999999, highest_price: 1000000, product_name: "무풍에어컨 클래식 (58.5 ㎡ + 18.7 ㎡) + BESPOKE 큐브™ Air (53 ㎡)" },
+  {auction_id: 70, auction_title: "삼성전자 ❄무풍 에어컨❄", category_name: "BESPOKE", cnt: 888888, highest_price: 2000000, product_name: "무풍에어컨 클래식 (58.5 ㎡ + 18.7 ㎡) + BESPOKE 큐브™ Air (53 ㎡)" },
+  {auction_id: 70, auction_title: "삼성전자 ❄무풍 에어컨❄", category_name: "BESPOKE", cnt: 777777, highest_price: 3000000, product_name: "무풍에어컨 클래식 (58.5 ㎡ + 18.7 ㎡) + BESPOKE 큐브™ Air (53 ㎡)" },
+  {auction_id: 70, auction_title: "삼성전자 ❄무풍 에어컨❄", category_name: "BESPOKE", cnt: 666666, highest_price: 4000000, product_name: "무풍에어컨 클래식 (58.5 ㎡ + 18.7 ㎡) + BESPOKE 큐브™ Air (53 ㎡)" },
+  {auction_id: 70, auction_title: "삼성전자 ❄무풍 에어컨❄", category_name: "BESPOKE", cnt: 555555, highest_price: 5000000, product_name: "무풍에어컨 클래식 (58.5 ㎡ + 18.7 ㎡) + BESPOKE 큐브™ Air (53 ㎡)" },
+];
